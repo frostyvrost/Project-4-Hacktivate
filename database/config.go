d18e8a5264ea9a0866ceec99239782d641624d81
Atlas <frostyvrost12@gmail.com>
2023-12-01 05:46:45 +0700

    config updated

--- database/config.go
+++ database/config.go
@@ -9,11 +9,17 @@ import (
 )
 
 func GetDBConfig() string {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("error loading .env file", err)
+		}
 	}
+
+	// err := godotenv.Load()
+
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
